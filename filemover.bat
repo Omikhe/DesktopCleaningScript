b12a4e81a6284b9f44cb9726e8ced00c1e81dc0c
Mike Andrew Msaka <msakamike84@gmail.com>
2023-05-13 13:25:30 +0200

    Call config file in main script

--- filemover.bat
+++ filemover.bat
@@ -3,6 +3,11 @@
 REM Navigate to the desktop directory
 cd "%userprofile%\Desktop"
 
+REM Read default commands from configuration file
+for /f "tokens=* delims=" %%a in (config.txt) do (
+    call %%a
+)
+
 REM Parse command-line arguments
 set "filetype=%1"
 set "destination=%2"
