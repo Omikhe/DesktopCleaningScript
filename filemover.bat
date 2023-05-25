9a9e4de288cedccc84018552fa895d61bd67df7d
Mike Andrew Msaka <msakamike84@gmail.com>
2023-05-25 23:01:30 +0200

    Attempt user input flexibility and add a bunch of comments

--- filemover.bat
+++ filemover.bat
@@ -1,18 +1,38 @@
 @echo off
 
-REM Navigate to the desktop directory
-cd "%userprofile%\Desktop"
-
-REM Read default commands from configuration file
-for /f "tokens=* delims=" %%a in (config.txt) do (
-    call %%a
-)
+REM Keep current working directory
+set chilembo=%~d0
+set inisho_foda=%~dp0
 
 REM Parse command-line arguments
-set "filetype=%1"
-set "destination=%2"
-if not defined filetype set "filetype=*.*"
-if not defined destination set "destination=%userprofile%\Documents"
+set "filetype=%1"    REM Specify the file type to be moved
+set "destination=%2" REM Specify the destination folder
 
-REM Move files to the appropriate folder
-move *.%filetype% "%destination%"
+REM Check if both command-line arguments are defined
+REM If yes, perform a single-file-type move
+REM If not, execute default commands
+if not "%filetype%"=="" if not "%destination%"=="" (
+	
+	REM Navigate to the desktop directory
+	C:
+	cd "%userprofile%\Desktop"
+
+	REM Move files of the specified file type to the destination folder
+	echo Moving files...
+	move *.%filetype% "%destination%"
+	
+	REM return to intial working directory
+	%chilembo% && cd %inisho_foda%
+
+) else (
+	
+	echo Hello
+	REM Read default commands from the configuration file
+	for /f "tokens=* delims=" %%a in (config.txt) do (
+    		call %%a
+	)
+	
+	REM return to intial working directory
+	%chilembo% && cd %inisho_foda%
+
+)
